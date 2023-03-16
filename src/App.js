a0f185bad5f98ff411bfcd83a06637b1b824ebf6
Jimi <jimi_p85@hotmail.com>
2023-03-16 19:52:45 -0400

    react practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header({title, num}) {
+  return <h1>Hello World {num} {title}</h1>;
 }
+
+function App({title, num}) {
+  return (
+    <Header title={title} num={num}/>
+  )
+};
 
 export default App;
